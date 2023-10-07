d6982d228650d962f36fd07f4eaf24c6a6fe50b7
Pasindu Prabhashitha <pasinduprabashitha@gmail.com>
2023-10-07 14:38:50 +0530

    feat: add cors policy

--- Program.cs
+++ Program.cs
@@ -10,10 +10,16 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddApplicationInsightsTelemetry();
 builder.Host.UseSerilog();
 
+builder.Services.AddCors(p => p.AddPolicy("cors", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+
 RegisterServicesConfig.InjectServices(builder.Services);
 
 var app = builder.Build();
 
+app.UseCors("cors");
 app.UseSerilogRequestLogging();
 app.UseSwagger();
 app.UseSwaggerUI();
