b02ff511a55cceca5ecc044804b3adc790741885
Pasindu Prabhashitha <pasinduprabashitha@gmail.com>
2023-10-12 22:59:46 +0530

    feat: change reservation dto

--- Dtos/Reservation/CreateReservationDto.cs
+++ Dtos/Reservation/CreateReservationDto.cs
@@ -12,5 +12,7 @@ namespace TicketEase.Dtos.Reservation
         public string FromStationId { get; set; } = null!;
         [Required]
         public string ToStationId { get; set; } = null!;
+        public DateTime Date { get; set; } = DateTime.Now;
+        public int PassengerCount { get; set; } = 0;
     }
 }
